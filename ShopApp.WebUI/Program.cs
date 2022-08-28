03de7015ea6386224de31fbbe3232ee0863aef45
Altan Emre <altan_emre_1989@hotmail.com>
2022-08-28 18:40:09 +0300

    IdentityUser

--- ShopApp.WebUI/Program.cs
+++ ShopApp.WebUI/Program.cs
@@ -1,14 +1,59 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using ShopApp.Business.Abstract;
 using ShopApp.Business.Concrete;
 using ShopApp.DataAccess.Abstract;
 using ShopApp.DataAccess.Concrete.EfCore;
 using ShopApp.DataAccess.Concrete.Memory;
+using ShopApp.WebUI.Identity;
 using ShopApp.WebUI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationIdentityDbContext>(options =>
+options.UseSqlServer(@"Server=DESKTOP-9IJKPL9\SQLDERS; Database=ShopApp; uid=sa;pwd=1"));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+    .AddDefaultTokenProviders();
+
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    //password
+    options.Password.RequireDigit = true;  
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 6;
+
+    options.Lockout.MaxFailedAccessAttempts = 5; //5 kere yanlýþ giriþte kitle
+    options.Lockout.DefaultLockoutTimeSpan=TimeSpan.FromMinutes(5); //5 kere yanlýþ giriþte 5 dakika kitle
+    options.Lockout.AllowedForNewUsers = true;
+
+    //user
+    options.User.RequireUniqueEmail = true;
+
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+    options.SignIn.RequireConfirmedEmail = true;
+
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";              //Giriþ Sayfasý
+    options.LogoutPath = "/Account/Logout";            //Çýkýþ Sayfasý
+    options.AccessDeniedPath = "/account/accessdenied";//Yetkisiz Giriþ Sayfasý
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60); //Cookie Süresi(default 20dk)
+    options.SlidingExpiration = true;
+    options.Cookie = new CookieBuilder
+    {
+        HttpOnly = true,                               //Tarayýcýda tutulsun
+        Name = "ShopApp.Security.Cookie"               //Cookie adý
+    };
+});
 
 
 //AddScoped: Gelen her bir web request için bir instance oluþturur ve gelen her ayný request te ayný instance'ý kullanýlýr, farklý web requestleri içinde yeniden instance alýr.
@@ -42,7 +87,7 @@ else
 }
 app.UseStaticFiles();
 app.CustomStaticFiles(); //middleWare: Bootstrap kütüphanesini npm ile indirilecek ve nodemodules içerisindeki static dosyalarý ile dýþarýya açma iþlemidir.
-
+app.UseAuthentication(); //Sýralama Önemli
 app.UseRouting();
 
 app.UseAuthorization();
